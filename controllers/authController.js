9821b26f1cbf03e7d38002d3eacd335b9364e461
Vaso <vasothom@hotmail.com>
2023-02-01 16:02:26 +0100

    aufgabe2

--- controllers/authController.js
+++ controllers/authController.js
@@ -13,22 +13,34 @@ export const register = async (req, res, next) => {
   res.send(newUser);
 };
 
-export const login = async (req, res) => {
-  // Finde ein Objekt mit dem selben Usernamen:
-  const user = await userModel.findOne({ name: req.body.name });
-  if (!user) {
-    res.send("No user found!");
-    return;
-  }
-  // Ich vergleiche das passwort aus dem Body mit dem aus der Datenbank.
-  const passwordIsEqual = await bcrypt.compare(
-    req.body.password,
-    user.password
-  );
-  console.log(passwordIsEqual);
-  if (passwordIsEqual) {
-    res.send("login success");
-  } else {
-    res.sendStatus(403);
+export const login = async (req, res, next) => {
+  try {
+    // Finde ein Objekt mit dem selben Usernamen:
+    const user = await userModel.findOne({ name: req.body.name });
+    if (!user) {
+      res.send("No user found!");
+      return;
+    }
+    // Ich vergleiche das passwort aus dem Body mit dem aus der Datenbank.
+    const passwordIsEqual = await bcrypt.compare(
+      req.body.password,
+      user.password
+    );
+    console.log(passwordIsEqual);
+    if (passwordIsEqual) {
+      //create token
+      const token = signToken({ id: user.id, email: user.email });
+      // add cookies
+      const expDate = 1000 * 60 * 60 * 24 * 365;
+      res.cookie("singIn", token, { maxAge: expDate, httpOnly: true });
+      //cookies zurück
+      res.json({ message: "login success", id: user._id });
+    } else {
+      const err = new Error("Login failed");
+      err.statusCode = 403;
+      throw err;
+    }
+  } catch (err) {
+    return next(err);
   }
 };
